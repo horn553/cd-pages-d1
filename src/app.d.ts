6d3f8bf0ea80fc7de776f00b44c869321669603d
horn553 <36446753+horn553@users.noreply.github.com>
2024-11-02 00:09:43 +0900

    Revert "configure basic d1 settings"
    This reverts commit 41ac78a3ffd44090a020c22b978f4e7ade77b9e2.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,12 @@
-import type { D1Database } from '@cloudflare/workers-types';
-
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
 declare global {
 	namespace App {
-		interface Platform {
-			env: {
-				DB: D1Database;
-			};
-			context: {
-				waitUntil(promise: Promise<unknown>): void;
-			};
-			caches: CacheStorage & { default: Cache };
-		}
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
 	}
 }
 
